b2fc660c79544dde68840f7ee8d81e1d23167b74
Jason <jasonshepherd@jasons-mbp>
2017-12-29 12:10:59 -0500

    Cleaned up filter function

--- src/main/java/com/example/demo/SocialApplication.java
+++ src/main/java/com/example/demo/SocialApplication.java
@@ -43,14 +43,12 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// @formatter:off
 		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
 				.authenticated().and().exceptionHandling()
 				.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/")).and().logout()
 				.logoutSuccessUrl("/").permitAll().and().csrf()
 				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
 				.addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
-		// @formatter:on
 	}
 
 	public static void main(String[] args) {
